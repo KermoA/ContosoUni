222735305cacf7b8cb626bd99cbb09346a24b2b5
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-09-10 10:26:17 +0300

    DeleteAndEditViews

--- ContosoUni/ContosoUni/Controllers/StudentsController.cs
+++ ContosoUni/ContosoUni/Controllers/StudentsController.cs
@@ -75,5 +75,106 @@ namespace ContosoUni.Controllers
 			return View();
 		}
 
+		[HttpGet]
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students.FindAsync(id);
+			if (student == null) 
+			{
+				return NotFound();
+			}
+
+			return View(student);
+		}
+
+		[HttpPost, ActionName("Edit")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> EditPost(int? id)
+		{
+			if (id == null) 
+			{
+				return NotFound();
+			}
+			var studentToUpdate = await _context.Students
+				.FirstOrDefaultAsync(s => s.ID == id);
+
+			if (await TryUpdateModelAsync<Student>(
+				studentToUpdate,
+				"",
+				s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+			{
+				try
+				{
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+				catch (DbUpdateException)
+				{
+					ModelState.AddModelError("", "Unable to save changes. " +
+						"Try again, and if the problem persists, " +
+						"see your system administrator.");
+				}
+			}
+
+			return View(studentToUpdate);
+		}
+
+		[HttpGet]
+
+		public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+		{
+            if (id == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students
+				.AsNoTracking()
+				.FirstOrDefaultAsync(m => m.ID == id);
+
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			if (saveChangesError.GetValueOrDefault())
+			{
+				ViewData["ErrorMessage"] = "Delete failed. " +
+					"Try again, and if the problem persists, " +
+					"see your system administrator.";
+			}
+
+            return View(student);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			//var student = await _context.Students.FindAsync(id);
+			//if (student == null)
+			//{
+			//	return RedirectToAction(nameof(Index));
+			//}
+
+			try
+			{
+				Student studentToDelete = new Student() { ID = id };
+				_context.Entry(studentToDelete).State = EntityState.Deleted;
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			catch (DbUpdateException)
+			{
+				return RedirectToAction(nameof(Delete), new {id = id, saveChangesError = true});
+			}
+
+			
+		}
 	}
 }
