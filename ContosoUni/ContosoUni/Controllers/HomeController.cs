7804352f82ca630ec3a2d8c64267a494df01750c
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-09-16 10:35:38 +0300

    AboutViewAndCodeFirstMigration

--- ContosoUni/ContosoUni/Controllers/HomeController.cs
+++ ContosoUni/ContosoUni/Controllers/HomeController.cs
@@ -1,32 +1,55 @@
+using ContosoUni.Data;
 using ContosoUni.Models;
+using ContosoUni.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ContosoUni.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		public readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController
+			(
+			ILogger<HomeController> logger,
+			SchoolContext context
+			)
+		{
+			_logger = logger;
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public async Task<IActionResult> About()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Students
+				group student by student.EnrollmentDate
+				into dateGroup
+				select new EnrollmentDateGroup()
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+			return View(await data.AsNoTracking().ToListAsync());
+		}
+	}
 }
