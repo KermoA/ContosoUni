ebaec09d593d8be7df1d06f1511ce21a13f33338
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-09-17 10:54:38 +0300

    CourseCRUD

--- ContosoUni/ContosoUni/Models/Course.cs
+++ ContosoUni/ContosoUni/Models/Course.cs
@@ -16,7 +16,9 @@ namespace ContosoUni.Models
         public int Credits { get; set; }
 
         public int DepartmentId { get; set; }
+        public Department Department { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
